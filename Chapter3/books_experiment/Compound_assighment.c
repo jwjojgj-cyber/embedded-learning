ba3184065d7489454e95d492523752953dbb5fc2
jwjojgj-cyber <jwjojgj@gmail.com>
2025-08-22 22:09:43 +0300

    Create Compound_assighment.c

---
+++ Chapter3/books_experiment/Compound_assighment.c
@@ -0,0 +1,18 @@
+
+// compound Assighment(side efect) difference
+ 
+#include <stdio.h>
+
+int main(void) {
+    int a[3] = {10, 20, 30};
+    int i = 0;
+
+    a[i++] += 5;   // compound assignment
+    printf("a[0]=%d, a[1]=%d, i=%d\n", a[0], a[1], i);
+
+    i = 0;         // reset i
+    a[i++] = a[i++] + 5;   // looks "the same"
+    printf("a[0]=%d, a[1]=%d, i=%d\n", a[0], a[1], i);
+
+    return 0;
+}
