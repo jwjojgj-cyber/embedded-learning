66369db2dd13159897867dbceed20536c3570dda
jwjojgj-cyber <jwjojgj@gmail.com>
2025-10-07 18:21:58 +0300

    chapter 7

--- Chapter7/projects/4.c
+++ Chapter7/projects/4.c
@@ -3,9 +3,37 @@
 int main(void){
     char ch;
     printf("Enter your number: ");
-    while(getchar() != '\n'){
-    ch = getchar();
+     
+    while((ch = getchar()) != '\n'){
+    
     if(ch>47 && ch < 58 || ch == 45 )
-        printf("ch");
+        printf("%c", ch);
+    else if(ch>64 && ch < 91 )
+        switch(ch){
+            case 'A': case 'B': case 'C':
+                 printf("2"); 
+                 break;
+            case 'D': case 'E': case 'F':
+                 printf("3"); 
+                 break;
+            case 'G': case 'H': case 'I':
+                  printf("4"); 
+                  break;
+            case 'J': case 'K': case 'L':
+                 printf("5"); 
+                 break;
+            case 'M': case 'N': case 'O':
+                 printf("6"); 
+                 break;
+            case 'P': case 'R': case 'S':
+                  printf("7"); 
+                  break;
+            case 'T': case 'U': case 'V':
+                 printf("8"); 
+                 break;
+            case 'W': case 'X': case 'Y':
+                 printf("9"); 
+                 break;
+        }
     }
 }
