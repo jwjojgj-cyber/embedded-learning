895ed062c9956e120c72bc1a04c0bd1242352546
jwjojgj-cyber <jwjojgj@gmail.com>
2025-10-30 16:21:23 +0200

    ch7

--- Chapter7/projects/11_Name.c
+++ Chapter7/projects/11_Name.c
@@ -2,17 +2,49 @@
 #include <ctype.h>
 
 int main(void){
-    char c, ch;
+    char c, firstInitial;
     printf("Enter a first and last name: ");
     scanf(" %c", &c);
+    firstInitial = c;
 
     while(getchar() != ' ');
-    
+    while((c=getchar()) == ' ');
     do
     {
-        scanf(" %c", &ch);
-        printf("%c", ch);
-    } while (ch != '\n');
-    printf("%c", c);
+        putchar(c);
+        c = getchar();
+    } while (c != '\n');
+    printf(", %c", firstInitial);
 
 }
+/*
+#include <stdio.h>
+
+int main(void) {
+    char c, first_initial;
+
+    printf("Enter a first and last name: ");
+
+    // Skip initial spaces
+    while ((c = getchar()) == ' ');
+
+    first_initial = c; // Save first letter
+
+    // Skip rest of first name
+    while ((c = getchar()) != ' ' && c != '\n');
+
+    // Skip spaces before last name
+    while ((c = getchar()) == ' ');
+
+    // Print last name
+    do {
+        putchar(c);
+        c = getchar();
+    } while (c != ' ' && c != '\n');
+
+    // Print result
+    printf(", %c.\n", first_initial);
+
+    return 0;
+}
+    */
---
+++ Chapter7/projects/12_calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float num1, num2;
+    char opperator;
+
+    printf("Enter an expression: ");
+    scanf("%f", &num1)
+}
