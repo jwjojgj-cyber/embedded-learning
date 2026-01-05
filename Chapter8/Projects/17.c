02f389967b2f9627f389800a322ffb6067b43f39
jwjojgj-cyber <jwjojgj@gmail.com>
2026-01-05 02:38:25 +0200

    finished arrays

--- Chapter8/Projects/17.c
+++ Chapter8/Projects/17.c
@@ -2,23 +2,62 @@
 
 int main(){
     int size = 0;
-    printf("This program creates a magic square of a specified size.\n
-        THe size must be an odd number betrween 1 and 99.\n
-        Enter the size of magic square: ");
+    printf("This program creates a magic square of a specified size.\n"
+            "The size must be an odd number betrween 1 and 99.\n"
+            "Enter the size of magic square: ");
         while(1){
-            scanf("%d" &size)
-            if(size < 0 || size > 99){
-                printf("Value is out of sight");
+            printf("Enter the size of magic square: ");
+            if (scanf("%d", &size) != 1) {
+                printf("Value is not a number\n");
                 while(getchar() != '\n')
                 ;
                 continue;
             }
-            if(size % 2 == 1){
-                printf("Value is not odd");
+            if(size <= 0 || size > 99){
+                printf("Value is out of sight\n");
                 while(getchar() != '\n')
                 ;
                 continue;
             }
+            if(size % 2 == 0){
+                printf("Value is not odd\n");
+                while(getchar() != '\n')
+                ;
+                continue;
+            }
+            
+            break;
         }
-    int square;
+
+    int fe[size][size];
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            fe[i][j] = 0;
+        }
+    }
+
+
+     int x = 0;
+     int y = (size/2);
+
+    for(int i = 1; i <= size*size; i++){
+
+        fe[x][y] = i;
+
+        int new_x = ((x - 1)+ size)%size;
+        int new_y = (y + 1) %size;
+        if(fe[new_x][new_y] == 0){
+        x = new_x;
+        y = new_y;
+        }
+        else
+        x = (x + 1) % size;
+    }
+
+    for(int i = 0; i < size; i++){
+        printf("\n");
+        for(int j = 0; j < size; j++){
+            printf("   %d", fe[i][j]);
+        }
+    }
 }
